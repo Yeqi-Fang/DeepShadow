b41dc655a3907b372f39df83add87b0340255882
fangyeqi_win <103356538+Yeqi-Fang@users.noreply.github.com>
2024-02-14 23:32:47 +0800

    move func in yolov5.py to utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import torch
 import random
+import os
+import subprocess
 try:
     from healpy.newvisufunc import projview, newprojplot
 except:
@@ -211,3 +213,53 @@ def labels_plot(image_paths, label_paths, num_samples, curr_dir):
     plt.savefig(f'{curr_dir}/label_plot.png', dpi=600)
     # plt.show()
     
+def set_res_dir(TRAIN):
+    # Directory to store results
+    res_dir_count = len(glob.glob('runs/train/*'))
+    print(f"Current number of result directories: {res_dir_count}")
+    if TRAIN:
+        RES_DIR = f"results_{res_dir_count+1}"
+        print(RES_DIR)
+    else:
+        RES_DIR = f"results_{res_dir_count}"
+    return RES_DIR
+
+
+def show_valid_results(RES_DIR):
+    # !ls runs/train/{RES_DIR}
+    print(os.listdir(f"runs/train/{RES_DIR}"))
+    EXP_PATH = f"runs/train/{RES_DIR}"
+    validation_pred_images = glob.glob(f"{EXP_PATH}/*_pred.jpg")
+    print(validation_pred_images)
+    for pred_image in validation_pred_images:
+        image = cv2.imread(pred_image)
+        plt.figure(figsize=(19, 16))
+        plt.imshow(image[:, :, ::-1])
+        plt.axis('off')
+        plt.show()
+
+
+# Helper function for inference on images.
+def inference(RES_DIR, data_path):
+    # Directory to store inference results.
+    infer_dir_count = len(glob.glob('runs/detect/*'))
+    print(f"Current number of inference detection directories: {infer_dir_count}")
+    INFER_DIR = f"inference_{infer_dir_count+1}"
+    print(INFER_DIR)
+    # Inference on images.
+    subprocess.run(f'python detect.py --weights runs/train/{RES_DIR}/weights/best.pt '
+                f'--source {data_path} --name {INFER_DIR}', cwd='yolov5', capture_output=True)
+    return INFER_DIR
+
+
+def visualize(INFER_DIR):
+# Visualize inference images.
+    INFER_PATH = f"runs/detect/{INFER_DIR}"
+    infer_images = glob.glob(f"{INFER_PATH}/*.jpg")
+    print(infer_images)
+    for pred_image in infer_images:
+        image = cv2.imread(pred_image)
+        plt.figure(figsize=(19, 16))
+        plt.imshow(image[:, :, ::-1])
+        plt.axis('off')
+        plt.show()
